b19f2cf05fcd8d9c51d8b2448852ac490a5f68c2
thaverk <thaverk01@gmail.com>
2023-12-11 14:19:03 +0200

    Assignment #3

---
+++ Wireless Earphones/Buds.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Wireless_Earphones
+{
+    internal class Buds
+    {
+
+        public string powerON;
+        public string brand;
+        public string colour;
+        public string bluetooth;
+
+        
+
+
+
+
+
+
+
+        public Buds(bool PowerOn, bool Bluetooth, string Brand, string Color)
+        {
+            brand = Brand;
+           colour = Color;
+
+            if (Bluetooth)
+            {
+                bluetooth = "Bluetooth is connected";
+            }
+            else
+            {
+                bluetooth = "Bluetooth is not connected";
+            }
+
+            if (PowerOn)
+            {
+                powerON = "Power is on";
+            }
+            else
+            {
+                powerON = "Power is off";
+            }
+
+
+
+        }
+
+        
+
+
+
+
+    }
+}
---
+++ Wireless Earphones/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Wireless_Earphones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Buds earphones = new Buds(true,true,"Samsung","Red");
+
+           Console.WriteLine($"Welcome to {earphones.brand} Digital Well-Being Statiscs: ");
+            Console.WriteLine($"Power: {earphones.powerON}");
+            Console.WriteLine($"Bluetooth: {earphones.bluetooth}");
+            Console.WriteLine($"The colour of your phone is: {earphones.colour}");
+            
+  
+        }
+    }
+}
